ccab2aa8032133ec4943738db91fc3f5c8afaaec
ruslanvs <ruslanvs@yahoo.com>
2018-02-09 15:48:14 -0800

    Assignment complete

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var app = express();
 
 var bodyParser = require('body-parser');
 var path = require("path");
-// var session = require( "express-session" );
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./static")));
@@ -12,21 +10,22 @@ app.use(express.static(path.join(__dirname, "./static")));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-// app.use( session( {secret: 'codingdojorocks'} ) )
-
 app.get('/', function(req, res) {
     res.render( "index" );
 })
 
-// app.post('/surveys/create', function(req, res) {
-//     req.session.survey = req.body
-//     res.redirect('/surveys/show');
-// })
-
-// app.get('/surveys/show', function(req, res) {
-//     res.render('survey', {survey: req.session.survey});
-// })
-
-app.listen(8000, function() {
-    console.log("listening on port 8000");
+var server = app.listen( 8000, function() {
+    console.log( "listening on port 8000" );
 });
+
+var io = require( "socket.io" ).listen( server );
+
+io.sockets.on( "connection", function( socket ){
+    console.log( "Client/socket is connected")
+    console.log( "Client/socket id is: ", socket.id );
+    
+    socket.on( "form_submit", function( data ){
+        data.lucky_nr = Math.ceil( Math.random()*1000  );
+        socket.emit( "server_response", { response: data } );
+    })
+})
